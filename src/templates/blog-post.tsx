f938ed0ec5a82321f909b34bbd38fa7e16926277
webdevcrazier <devcrazier@gmail.com>
2020-01-14 22:12:00 +0200

    Changed components and tamplate

--- src/templates/blog-post.tsx
+++ src/templates/blog-post.tsx
@@ -36,7 +36,7 @@ export interface IBlogPostFrontmatter {
 
 export interface IBlogPostData {
   id: string;
-  htmlAst: string;
+  html: string;
   timeToRead: string;
   fields: {
     slug: string;
@@ -79,7 +79,7 @@ export const pageQuery = graphql`
     markdownRemark(fields: { slug: { eq: $slug } }) {
       id
       excerpt(format: HTML)
-      htmlAst
+      html
       timeToRead
       fields {
         slug
