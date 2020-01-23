48144db36738048f0a643c972bfabbd008bb795e
Super Appdev <frontexport1987@gmail.com>
2020-01-23 13:14:45 +0200

    feat: add gatsby-remaker-embedder

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,8 @@ const path = require('path');
 const dvc = require('./config/prismjs/dvc');
 
 const title = 'Data Version Control · DVC';
-const keywords = 'git, data, version control, machine learning models management, datasets';
+const keywords =
+  'git, data, version control, machine learning models management, datasets';
 const description =
   'Data Version Control Blog. We write about machine learning workflow. From data versioning and processing to model productionization. We share our news, findings, interesting reads, community takeaways.';
 
@@ -40,6 +41,9 @@ const plugins = [
     resolve: 'gatsby-transformer-remark',
     options: {
       plugins: [
+        {
+          resolve: 'gatsby-remark-embedder'
+        },
         {
           resolve: 'gatsby-remark-embed-gist',
           options: {
