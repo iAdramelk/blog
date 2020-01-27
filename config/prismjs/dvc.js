5a5e88520df801b9783bbd2c0e7f198143a8019f
Fábio Santos <fabiosantosart@gmail.com>
2020-01-27 19:20:49 +0000

    improve highlighting

--- config/prismjs/dvc.js
+++ config/prismjs/dvc.js
@@ -92,18 +92,82 @@ const dvc = [
   'add'
 ];
 
+const keywords = [
+  'while',
+  'until',
+  'time',
+  'then',
+  'select',
+  'in',
+  'if',
+  'function',
+  'for',
+  'fi',
+  'esac',
+  'else',
+  'elif',
+  'done',
+  'do',
+  'case'
+];
+
+const beforeCommand = String.raw`(\$[\s(]+|;\s*)`;
+
+const variable = {
+  pattern: /\$[a-zA-Z_][a-zA-Z_0-9]*/,
+  greedy: true
+};
+
+const comment = {
+  pattern: /#.*/,
+  greedy: true
+};
+
+const insideLine = {
+  command: {
+    pattern: new RegExp(
+      String.raw`${beforeCommand}\b(?:${commands.concat(keywords).join('|')})\b`
+    ),
+    greedy: true,
+    lookbehind: true
+  },
+  git: {
+    pattern: new RegExp(
+      String.raw`${beforeCommand}\b(?:git (?:${git.join('|')}))\b`
+    ),
+    greedy: true,
+    lookbehind: true
+  },
+  dvc: {
+    pattern: new RegExp(
+      String.raw`${beforeCommand}\b(?:dvc (?:${dvc.join('|')}))\b`
+    ),
+    greedy: true,
+    lookbehind: true
+  },
+  string: {
+    pattern: /(["'])(?:\\(?:\r\n|[\s\S])|(?!\1)[^\\\r\n])*\1/,
+    greedy: true,
+    inside: {
+      variable: { ...variable }
+    }
+  },
+  variable
+};
+
+const subcommand = {
+  pattern: /\$\((.+)\)/,
+  inside: { ...insideLine },
+  alias: 'important'
+};
+
 module.exports = {
+  comment: { ...comment },
   line: {
     pattern: /(?<=(^|\n))\$[\s\S]*?[^\\](:?\n|$)/,
     inside: {
-      input: /^\$\s+/,
-      string: {
-        pattern: /(["'])(?:\\(?:\r\n|[\s\S])|(?!\1)[^\\\r\n])*\1/,
-        greedy: true
-      },
-      command: new RegExp(`\\b(?:${commands.join('|')})\\b`),
-      git: new RegExp(`git \\b(?:${git.join('|')})\\b`),
-      dvc: new RegExp(`dvc \\b(?:${dvc.join('|')})\\b`)
+      subcommand,
+      ...insideLine
     }
   }
 };
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,20 @@
 const path = require('path');
 const dvc = require('./config/prismjs/dvc');
+const Prism = require('prismjs');
+
+// Make sure the $ part of the command prompt in shell
+// examples isn't copiable by making it an 'input' token.
+Prism.hooks.add('after-tokenize', env => {
+  if (env.language !== 'dvc') return;
+  debugger;
+  const { tokens } = env;
+  for (let i = 0; i < tokens.length; i++) {
+    if (tokens[i].type === 'line' && /\$\s?/.test(tokens[i].content[0])) {
+      const old = tokens[i].content[0];
+      tokens[i].content[0] = new Prism.Token('input', '$ ', null, '$ ', false);
+    }
+  }
+});
 
 const title = 'Data Version Control · DVC';
 const keywords =
