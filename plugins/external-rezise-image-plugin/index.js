d1248e5fee6fa00d32d39251de0426726ad22eaf
Super Appdev <frontexport1987@gmail.com>
2020-02-03 20:30:38 -0300

    remove set height

--- plugins/external-rezise-image-plugin/index.js
+++ plugins/external-rezise-image-plugin/index.js
@@ -26,20 +26,19 @@ module.exports = ({ markdownAST }) => {
 
     wrapperImageList.forEach(wrapperImage => {
       const image = select('.gatsby-resp-image-image', wrapperImage)
-      const backgroundImage = select('.gatsby-resp-image-background-image', wrapperImage)
 
       const title = image
         .properties
         .title
         .trim()
-        .replace(/=\d{2,4}x\d{2,4}|=\d{2,4}/gi, ' ')
+        .replace(/=\d{2,4}/gi, ' ')
         .trim();
 
       const resize = image
         .properties
         .title
         .trim()
-        .match(/=\d{2,4}x\d{2,4}|=\d{2,4}/gi);
+        .match(/=\d{2,4}/gi);
 
       const maxWidth = wrapperImage
         .properties
@@ -49,38 +48,16 @@ module.exports = ({ markdownAST }) => {
         .trim();
 
       if (!!resize) {
-        const [width, height] = resize[0].replace(/^(.)/g, '').split('x');
+        const width = resize[0].replace(/^(.)/g, '');
 
         image.properties.title = title ? title : image.properties.alt
 
         wrapperImage.properties.style = wrapperImage
           .properties
           .style
-          .replace(/(max-width: [0-9]{0,5}px)/g, `max-width: ${Number(width) > Number(maxWidth) ? maxWidth : width}px`);
-
-        if (!!height) {
-
-          backgroundImage.properties.style = backgroundImage
-            .properties
-            .style
-            .replace(/(padding-bottom: [0-9]{0,3}.[0-9]{0,100}%;)/g, `padding-bottom: ${height}px;`)
-
-          image.properties.style = image
-            .properties
-            .style
-            .replace(/(width:100%;)([\s\S]+)/g, `max-width:${Number(width) > Number(maxWidth) ? maxWidth : width}px; $2 object-fit: cover; overflow:hidden;`)
-
-        } else {
-
-          image.properties.style = image
-            .properties
-            .style
-            .replace(/(width:100%;)/g, `max-width:${Number(width) > Number(maxWidth) ? maxWidth : width}px;`)
-        }
+          .replace(/(max-width: \d{0,5}px)/g, `max-width: ${Number(width) > Number(maxWidth) ? maxWidth : width}px`);
       }
     })
     node.value = convertHastToHtml(hast)
   })
 };
-
-
