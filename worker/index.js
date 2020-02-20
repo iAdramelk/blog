235d5cfc294cc88e7aa1fdfe53fbce520fe7e41d
Ivan Shcheklein <shcheklein@gmail.com>
2020-02-20 13:44:59 -0800

    add all old posts redirects to CF worker

--- worker/index.js
+++ worker/index.js
@@ -1,6 +1,6 @@
 async function handleRequest(request) {
   let url = new URL(request.url);
-  let path = url.pathname.replace(/%EF%B8%8F/, '');
+  let path = url.pathname.replace(/%EF%B8%8F/, '').replace(/\/$/, '');
   let site = 'https://dvc.org';
   let blog = 'https://blog.dvc.org';
 
@@ -11,8 +11,17 @@ async function handleRequest(request) {
         return Response.redirect(site + '/doc/tutorials', 301);
       case '/data-version-control-beta-release-iterative-machine-learning-a7faf7c8be67':
         return Response.redirect(site + '/doc/tutorials', 301);
-      case '/november-19-dvc-heartbeat-110404842192':
-        return Response.redirect(blog + '/november-19-dvc-heartbeat/', 301);
+      case '/dvc-heartbeat-6301aebf5c96':
+        return Response.redirect(blog + '/march-19-dvc-heartbeat.md', 301);
+      case '/april19-dvc-heartbeat-296c71a59be4':
+        return Response.redirect(blog + '/april-19-dvc-heartbeat.md', 301);
+      case '/dvc-0-8-5-release-f66ef3b10684':
+        return Response.redirect(
+          'https://github.com/iterative/dvc/releases',
+          301
+        );
+      case /^\/.*-[a-z0-9]{12}$/.test(path):
+        return Response.redirect(blog + path.replace(/-[a-z0-9]$/, ''), 301);
     }
   }
 
